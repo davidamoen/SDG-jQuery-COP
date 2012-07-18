2edf57e2d935e1426ec45358f562266eedc5692a
David Moen <davidamoen@gmail.com>
2012-07-18 06:43:21 -0500

    moved to local jQuery files

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
-SDG-jQuery-COP
+SDG jQuery COP
 ==============
+
+Feel free to download this repo and use the jQuery examples for your own projects.  
--- js/scripts.js
+++ js/scripts.js
@@ -3,16 +3,10 @@
     colors = new Array('red', 'blue1', 'blue2', 'blue3', 'gray', 'yellow')
 
     $('#container').isotope({ filter: '.wrapper' }, function ($items) {
-        var id = this.attr('id'),
-        len = $items.length;
-        console.log('Isotope has filtered for ' + len + ' items in #' + id);
-
         $items.each(function () {
             var randomNum = Math.ceil(Math.random() * colors.length) - 1;
             $(this).find('.item').addClass(colors[randomNum]);
         });
-
-
     });
 });
 
