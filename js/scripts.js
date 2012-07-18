138d63d3e3bdf9e5effaff2771a589027bf3d1b7
David Moen <davidamoen@gmail.com>
2012-07-18 06:29:52 -0500

    initial commit

---
+++ js/scripts.js
@@ -0,0 +1,18 @@
+﻿$(document).ready(function () {
+
+    colors = new Array('red', 'blue1', 'blue2', 'blue3', 'gray', 'yellow')
+
+    $('#container').isotope({ filter: '.wrapper' }, function ($items) {
+        var id = this.attr('id'),
+        len = $items.length;
+        console.log('Isotope has filtered for ' + len + ' items in #' + id);
+
+        $items.each(function () {
+            var randomNum = Math.ceil(Math.random() * colors.length) - 1;
+            $(this).find('.item').addClass(colors[randomNum]);
+        });
+
+
+    });
+});
+
